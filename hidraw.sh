b71f27dc8693a2d7732cf41992ab5341fddafb4a
Ken Jin <jyinyu@intrepidcs.com>
2019-07-03 15:30:11 +0800

    added support for linux builds

---
+++ hidraw.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+#sudo apt-get install libudev-dev -y
+cd /etc/udev/rules.d
+sudo touch 99-hidraw-permissions.rules
+echo 'KERNEL=="hidraw*", SUBSYSTEM=="hidraw", MODE="0666"' >> 99-hidraw-permissions.rules
